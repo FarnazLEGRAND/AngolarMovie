b5f78f2d389d28b6246d83dca8528127c087f4d0
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-07-04 14:05:37 +0200

    delete

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -12,7 +12,13 @@ export class MovieService {
   fetchAll() {
     return this.http.get<Movie[]>('http://localhost:8000/api/movie');
   }
-  add(movie:Movie){
-    return this.http.post<Movie[]>('http://localhost:8000/api/movie',movie);
+
+  add(movie:Movie) {
+    return this.http.post<Movie>('http://localhost:8000/api/movie', movie);
+  }
+// on contaner id ou bien ecrire '/{$id}' ou '/'+id
+  delete(id:number) {
+    return this.http.delete<void>('http://localhost:8000/api/movie/'+id);
+
   }
 }
